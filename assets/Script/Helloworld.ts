f0e8e73d60664d6c0b81347d9043a83c72787175
kingBook <412126604@qq.com>
2018-11-15 10:34:46 +0800

    update 2018-11-15 10:34

--- assets/Script/Helloworld.ts
+++ assets/Script/Helloworld.ts
@@ -42,4 +42,60 @@ export default class Helloworld extends cc.Component {
 	public getUserAvatar(evetData:any):void{
 		this.label.string=(API4399.getInstance().getUserAvatar(this._uId));
 	}
+	
+	public showRanking(evetData:any):void{
+		API4399.getInstance().showRanking();
+	}
+	
+	public getRanking(evetData:any):void{
+		let page:number=1;
+		let step:number=10;
+		API4399.getInstance().getRanking((data:any):void=>{
+			if(data.code==10000){
+				this.label.string="getRanking获取成功";
+			}else if(data.code==10001){
+				this.label.string="getRanking获取失败";
+			}
+		},page,step);
+	}
+	
+	public submitRanking(evetData:any):void{
+		let score:number=(Math.random()*100)|0;
+		API4399.getInstance().submitRanking(score,(data:any):void=>{
+			if(data.code==10000){
+				this.label.string="submitRanking提交成功";
+			}else if(data.code==10001){
+				this.label.string="submitRanking提交失败";
+			}
+		});
+	}
+	
+	public getMyRanking(evetData:any):void{
+		API4399.getInstance().getMyRanking((data:any):void=>{
+			if(data.code==10000){
+				this.label.string="getMyRanking获取成功";
+			}else if(data.code==10001){
+				this.label.string="getMyRanking获取失败";
+			}
+		});
+	}
+	
+	public getNearRanking(evetData:any):void{
+		let step:number=10;
+		API4399.getInstance().getNearRanking((data:any):void=>{
+			if(data.code==10000){
+				this.label.string="getNearRanking获取成功";
+			}else if(data.code==10001){
+				this.label.string="getNearRanking获取失败";
+			}
+		},step);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
